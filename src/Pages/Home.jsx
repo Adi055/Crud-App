97a3694d50390d0f1503bb2289317ba1cd49ec91
Aditya tiwari <at257645@gmail.com>
2023-07-07 19:54:05 +0530

    fgs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,7 +24,7 @@ export default Home
 const DIV=styled.div`
     display:flex;
     .sidebar{
-        width:21%;
+        width:20%;
         position:relative;
         top:42px;
         box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
